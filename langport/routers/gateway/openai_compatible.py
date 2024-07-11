98fdc46721292c4c6c1a9289801ff2d019261872
jstzwj <1103870790@qq.com>
2024-07-11 21:54:35 +0800

    pydantic upgrade bug fix

--- langport/routers/gateway/openai_compatible.py
+++ langport/routers/gateway/openai_compatible.py
@@ -295,10 +295,10 @@ async def chat_completion_stream_generator(
                 if content.finish_reason is not None:
                     finish_stream_events.append(chunk)
                 continue
-            yield f"data: {chunk.json(exclude_unset=True, ensure_ascii=False)}\n\n"
+            yield f"data: {json.dumps(chunk.model_dump(exclude_unset=True), ensure_ascii=False)}\n\n"
     # There is not "content" field in the last delta message, so exclude_none to exclude field "content".
     for finish_chunk in finish_stream_events:
-        yield f"data: {finish_chunk.json(exclude_none=True, ensure_ascii=False)}\n\n"
+        yield f"data: {json.dumps(finish_chunk.model_dump(exclude_none=True), ensure_ascii=False)}\n\n"
     yield "data: [DONE]\n\n"
 
 
